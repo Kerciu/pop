e47b2482f3899461f8f6e35a19296753f31a0fd3
Ribbz <01187268@pw.edu.pl>
2025-11-07 12:26:35 +0100

    Fixed distance unit

--- src/core/distance_utils.py
+++ src/core/distance_utils.py
@@ -1,7 +1,7 @@
 from src.models.coordinate import Coordinate
 
 
-def distance(source: 'Coordinate', target: 'Coordinate') -> float:
+def distance(source: 'Coordinate', target: 'Coordinate') -> int:
     dx = source.x - target.x
     dy = source.y - target.y
     return (dx * dx + dy * dy) ** 0.5
